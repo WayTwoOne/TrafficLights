f7e46a551bff2612429251ca6d6f82644e9544dd
WayTwoOne <vladimirzakharovj@yandex.ru>
2023-08-16 05:02:49 -0700

    Update

--- TrafficLights/ViewController.swift
+++ TrafficLights/ViewController.swift
@@ -8,12 +8,52 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var redLight: UIView!
+    @IBOutlet weak var yellowLight: UIView!
+    @IBOutlet weak var greenLight: UIView!
+    
+    private let lightOff: CGFloat = 0.3
+    private let lightON: CGFloat = 1
+    
+    private let cornerRadius: CGFloat = 70
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        Configuration()
     }
 
 
+    @IBAction func turnOnLightButton() {
+        turnOnLight()
+    }
+    
+    private func Configuration() {
+        redLight.layer.cornerRadius = cornerRadius
+        redLight.backgroundColor = .systemRed
+        redLight.alpha = lightOff
+        yellowLight.layer.cornerRadius = cornerRadius
+        yellowLight.backgroundColor = .systemYellow
+        yellowLight.alpha = lightOff
+        greenLight.layer.cornerRadius = cornerRadius
+        greenLight.backgroundColor = .systemGreen
+        greenLight.alpha = lightOff
+
+    }
+    
+    private func turnOnLight() {
+        if redLight.alpha == lightOff, yellowLight.alpha == lightOff, greenLight.alpha == lightOff {
+            redLight.alpha = lightON
+        } else if redLight.alpha == lightON {
+            redLight.alpha = lightOff
+            yellowLight.alpha = lightON
+        } else if yellowLight.alpha == lightON {
+            yellowLight.alpha = lightOff
+            greenLight.alpha = lightON
+        } else if greenLight.alpha == lightON {
+            greenLight.alpha = lightOff
+            redLight.alpha = lightON
+        }
+    }
 }
 
